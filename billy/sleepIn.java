e8d7ef02ffa65b2c498d250bb07d191500215f01
billy <144733159+WBattist@users.noreply.github.com>
2026-04-11 02:13:30 +0000

    i dont have time today for leetcode, so i just did simple codingBat

---
+++ billy/sleepIn.java
@@ -0,0 +1,7 @@
+// i dont have time today for leetcode, so i just did simple codingBat
+public boolean sleepIn(boolean weekday, boolean vacation) {
+  if (!weekday || vacation){
+    return true;
+  }
+  return false;
+}
