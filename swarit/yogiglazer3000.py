3d08c9da1af22a810949a87a28c0c11522e32bdd
swaritchoudhariDSTEM <68472469+SwiftyProgrammer690@users.noreply.github.com>
2026-04-12 14:15:37 -0400

    yogi glaaazzee

---
+++ swarit/yogiglazer3000.py
@@ -0,0 +1,25 @@
+# hoyl yogi galze
+
+# so yogi is lowk gooning to nut rnow
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # lowkey this is not the most efficient way to do this but i could care less
+        # also lowkey this is not the most efficient way to do this but i could care less
+        # also lowkey this is not the most efficient way to do this but i could care less
+        # also lowkey this is not the most efficient way to do this but i could care less
+        # also lowkey this is not the most efficient way to do this but i could care less
+        # also lowkey this is not the most efficient way to do this but i could care less
+        # also lowkey this is not the most efficient way to do this but i could care less
+        # also lowkey this is not the most efficient way to do this but i could care less
+        # also lowkey this is not the most efficient way to do this but i could care less
+        # also lowkey this is not the most efficient way to do this but i could care less
+        # also lowkey this is not the most efficient way to do this but i could care less
+        # also lowkey this is not the most efficient way to do this but i could care less
+        # also lowkey this is not the most efficient way to do this but i could care less
+        # also lowkey this is not the most efficient way to do this but i could care less
+        # also lowkey this is not the most efficient way to do this but i could care less
+        s = s.lower()
+        s = ''.join(c for c in s if c.isalnum())
+        return s == s[::-1]
+    
