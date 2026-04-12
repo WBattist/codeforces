a2c7645201b534319bf3476fbd7edb36d730034e
Billy B <144733159+WBattist@users.noreply.github.com>
2026-04-11 21:38:30 -0400

    i am genuninely stupid ts took me 30 mins

---
+++ billy/DominoPiling.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class DominoPiling {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int m = scanner.nextInt();
+        int n = scanner.nextInt();
+
+        int maxDominoes = (m * n) / 2;
+        System.out.println(maxDominoes);
+
+        scanner.close();
+    }
+}
